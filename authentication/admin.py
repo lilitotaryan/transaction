4742d58b6fb688bc9048647e7a8f0fb17d7976a7
Lilit <lilit_otaryan@edu.aua.am>
2019-09-18 14:00:11 +0400

    [VIEW] admin view config

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
 from .models import VivaroUser
-admin.site.register(VivaroUser)
-#
-# @admin.register(VivaroUser)
-# class UserAdmin(admin.ModelAdmin):
-#         fieldsets = (
-#             (None, {
-#                 'fields': (('first_name', 'last_name'), 'username', 'email', 'password', 'phone_number', 'is_user', 'is_partner')
-#             }),
-#         ('Permissions',
-#          {'fields': (
-#              'is_user',
-#              'is_partner'
-#          )}),
-#         )
+
+@admin.register(VivaroUser)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('is_authenticated',)
