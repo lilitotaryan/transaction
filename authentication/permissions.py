dad6740f15a42178dc70ea710a4eaa007d869c77
Lilit <lilit_otaryan@edu.aua.am>
2019-09-23 13:56:25 +0400

    [FEATURE] set authentication permissions

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,21 +1,17 @@
 from rest_framework import permissions
 from .models import UserAction, Session
 
+class AlreadyAuthenticatedPermission(permissions.BasePermission):
+    message = 'User is already logged in'
+
+    def has_permission(self, request, view):
+            session = Session.objects.get(token=request.data.get("session_token"))
+            return session.action.loged_in
+
+
 class AuthenticatedPermission(permissions.BasePermission):
     message = 'No user is logged in'
 
     def has_permission(self, request, view):
-        try:
-            UserAction.objects.get(loged_in=True)
-            return True
-        except UserAction.DoesNotExist:
-            return False
-
-
-class PartnerPermission(permissions.BasePermission):
-    message = 'The user is not partner'
-
-    def has_object_permission(self, request, view, obj):
-        if request.methos in permissions.SAFE_METHODS:
-            return True
-        return obj.is_partner
+            session = Session.objects.get(token=request.data.get("session_token"))
+            return session.action.loged_out
--- authentication/views.py
+++ authentication/views.py
@@ -5,24 +5,28 @@ from django.core.exceptions import SuspiciousOperation
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserAction, VivaroUser, Session
+from .permissions import AuthenticatedPermission, AlreadyAuthenticatedPermission
 
-@api_view(['POST'])
-def registration(request):
-    data = UserRegestrationSerializer(data=request.data)
-    if data.is_valid():
-            try:
-                user = data.create(data.validated_data)
+
+class Registration(APIView):
+    permission_classses = [AlreadyAuthenticatedPermission]
+    def post(self, request):
+        data = UserRegestrationSerializer(data=request.data)
+        if data.is_valid():
+                try:
+                    data.create(data.validated_data)
+                except:
+                    return Response({"error": "User already exists"}, status=400)
                 return Response("Success")
-            except:
-                return Response({"error": "User already exists"}, status=400)
-    return Response({"error": "All the fields should be specified"}, status=400)
+        return Response({"error": "All the fields should be specified"}, status=400)
 
 
 @api_view(['POST'])
+@permission_classes([AlreadyAuthenticatedPermission])
 def login(request):
     data = VivaroUserLoginSerializer(data=request.data)
     if data.is_valid():
-        session = Session.objects.get(token = request.data.get("session_token"))
+        session = Session.objects.get(token=request.data.get("session_token"))
         user = data.authentication(data.validated_data)
         session.add_user(user)
         expired = session.is_unexpired()
@@ -34,60 +38,19 @@ def login(request):
                 session.update_last_date()
                 user.authenticate()
                 return Response({"username": user.username, "email": user.email, "phone_number": user.phone_number})
-            return Response({"error": "User is already logged in"}, status=402)
         if not user:
             return Response({"error": "Invalid username or password"}, status=404)
-        if expired:
-            return Response({"error": "User is already logged in"}, status=402)
     return Response({"error": "Invalid input"}, status=400)
 
 
 @api_view(['POST'])
+@permission_classes([AuthenticatedPermission])
 def logout(request):
-    session = Session.objects.get(token = request.data.get("session_token"))
+    session = Session.objects.get(token=request.data.get("session_token"))
     user = session.user
-    if not user or not session.action or not session.action.logged_in:
-        return Response({"error": "No user is logged in"}, status=404)
-    if not session:
-        return Response({"error": "No user is logged in anymore the session expired"}, status=404)
-    if user and session and session.is_unexpired() and session.action.logged_in:
+    if session.is_unexpired():
         session.action.logged_out()
         session.expire_all_sessions()
         user.unauthenticate()
         return Response("Success")
-
-
-class MoneyTransaction(APIView):
-    def put(self, request):
-        try:
-            session = Session.objects.get(token=request.data.get("session_token"))
-            user = session.user
-            UserAction.objects.get(loged_in=True)
-        except UserAction.DoesNotExist or VivaroUser.DoesNotExist or Session.DoesNotExist:
-            return Response({"error": "No user is logged in"}, status=404)
-        if user.is_partner:
-            data = MoneyTransactionSerializer(data=request.data)
-            if data.is_valid():
-                if not data.transfer(user, data.validated_data):
-                    return Response({"error": "Username does not exist"}, status=404)
-                return Response({"Success"})
-            return Response({"error": "Invalid input"}, status=400)
-        return Response({"error": "No partner is logged in"}, status=403)
-
-
-@api_view(['PUT'])
-def bonus_transaction(request):
-    try:
-        session = Session.objects.get(token=request.data.get("session_token"))
-        user = session.user
-        UserAction.objects.get(loged_in=True)
-    except UserAction.DoesNotExist or VivaroUser.DoesNotExist or Session.DoesNotExist:
-        return Response({"error": "No user is logged in"}, status=404)
-    if user.is_partner:
-        data = BonusTransactionSerializer(data=request.data)
-        if data.is_valid():
-            if not data.transfer(user, data.validated_data):
-                return Response({"error": "Username does not exist"}, status=404)
-            return Response({"Success"})
-        return Response({"error": "Invalid input"}, status=400)
-    return Response({"error": "No partner is logged in"}, status=403)
+    return Response({"error": "Session is already expired"}, status=404)
